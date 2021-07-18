06f3d94501d8ce8e8da1643997761c9a13b2ee5b
NaruDevnote <pokeraven4@gmail.com>
2021-07-19 00:02:14 +0100

    feat(api): switch to coupon manager

--- dashdactyl/api.py
+++ dashdactyl/api.py
@@ -1,6 +1,6 @@
 import requests
 from json import dumps
-from .managers import DashServerManager
+from .managers import DashServerManager, CouponManager
 from .structures import DashUser
 
 
@@ -14,6 +14,7 @@ class Dashdactyl:
         
         self.users = None # haven't created user manager yet
         self.servers = DashServerManager(self)
+        self.coupons = CouponManager(self)
     
     def request(self, method: str, path: str, params: dict={}) -> dict:
         if method not in ('GET', 'POST', 'DELETE'):
@@ -55,18 +56,3 @@ class Dashdactyl:
             return 'Invalid User ID'
         
         return DashUser(self, data)
-    
-    def create_coupon(self) -> str:
-        res = self.request('GET', '/create_coupon')
-        if 'status' in res:
-            return res
-        
-        return res['coupon']
-    
-    def revoke_coupon(self, code: str):
-        # This should be switched to the DELETE method...
-        return self.request('GET', f'/revoke_coupon?code={code}')
-    
-    def get_coupons(self):
-        # Would be nice to have this in Dashdactyl :/
-        return NotImplemented
