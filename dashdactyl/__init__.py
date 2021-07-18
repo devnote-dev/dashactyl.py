ab269bb09c3d0339efa21c0677024362fc6efb6d
NaruDevnote <pokeraven4@gmail.com>
2021-07-18 18:14:33 +0100

    refactor(pkg): remake this

--- dashdactyl/__init__.py
+++ dashdactyl/__init__.py
@@ -1,1 +1,16 @@
-from .ext import *
+'''# Dashdactyl.py
+### An interactive API Wrapper for Dashdactyl in Python
+
+Author: Devonte <https://github.com/devnote-dev>
+© 2021 devnote-dev
+License: MIT
+'''
+
+__title__ = 'dashdactyl'
+__author__ = 'Devonte'
+__copyright__ = 'MIT'
+__version__ = '0.0.1a'
+
+from .api import *
+from .managers import *
+from .structures import *
