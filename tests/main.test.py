246f588412040b067a3ca15e428dd7c698c43cc2
NaruDevnote <pokeraven4@gmail.com>
2021-07-19 16:10:27 +0100

    fix(test): update import & user test

--- tests/main.test.py
+++ tests/main.test.py
@@ -1,5 +1,7 @@
-from ..dashdactyl import Dashdactyl
+from ..dashactyl import Dashactyl
 
-dash = Dashdactyl('https://client.dynox.us', '$omeFak3AP1Key') # I'm not creative
-
+dash = Dashactyl('https://client.dynox.us', '$omeFak3AP1Key') # I'm not creative
 print(dash.ping())
+
+user = dash.users.get(622146791659405313) # that's me
+print(user.tag)
